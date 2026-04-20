ea38c36ce21d6362495d98678555484f790341f5
tianxinghe <976792132@qq.com>
2026-04-20 14:25:49 +0000

    task0

---
+++ src/task0/IR_optimize/include/IR_optimize.h
@@ -0,0 +1,12 @@
+//
+// Created by hby on 22-12-2.
+//
+
+#ifndef CODE_IR_OPTIMIZE_H
+#define CODE_IR_OPTIMIZE_H
+
+extern void IR_optimize1();
+extern void IR_optimize2();
+extern void IR_optimize3();
+
+#endif //CODE_IR_OPTIMIZE_H
